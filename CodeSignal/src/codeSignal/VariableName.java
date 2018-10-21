cb1dde824d0388b577f0435b8cc1fc25b18e262e
SunSpike <wjdehdgud321@naver.com>
2018-10-21 22:46:57 +0900

    Solved VariableName

---
+++ CodeSignal/src/codeSignal/VariableName.java
@@ -0,0 +1,17 @@
+package codeSignal;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class VariableName {
+	public static void main(String[] args){
+		Pattern pattern;
+		Matcher matcher;
+		
+		String name = "qq-q";
+		String regex = "[a-zA-Z$_][a-zA-Z0-9$_]*";
+		
+		pattern = Pattern.compile(regex);
+		matcher = pattern.matcher(name);
+	}
+}
