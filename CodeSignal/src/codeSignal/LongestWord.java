9299a542eb075461e91083fb34eb5c969014076f
SunSpike <wjdehdgud321@naver.com>
2018-12-04 09:00:40 +0900

    Solved LongestWord

--- CodeSignal/src/codeSignal/LongestWord.java
+++ CodeSignal/src/codeSignal/LongestWord.java
@@ -2,10 +2,19 @@ package codeSignal;
 
 public class LongestWord {
 	public static void main(String[] args){
-		String text = "Ready{{{, steady, go!";
+		String text = "Ready, Steady, Go!";
+		String[] array = text.split("[^a-zA-Z]");
 		
-		for ( int i = 0; i < text.length(); i++ ){
-			
+		int max = 0;
+		int index = 0;
+		
+		for( int i = 0; i < array.length; i++ ){
+			if ( max < array[i].length() ){
+				max = array[i].length();
+				index = i;
+			}
 		}
+		
+		System.out.println(array[index]);
 	}
 }
