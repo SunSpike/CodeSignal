bc9e4035e26854ca133e5e609bd72606ef0d8e54
SunSpike <wjdehdgud321@naver.com>
2018-12-27 15:06:05 +0900

    Solving SprialArray
    I hate this problem, It is really nice algorithm, but.. Hard to thinking

--- CodeSignal/src/codeSignal/SpiralArray.java
+++ CodeSignal/src/codeSignal/SpiralArray.java
@@ -1,5 +1,27 @@
 package codeSignal;
 
 public class SpiralArray {
-
+	public static void main(String[] args){
+		int n = 3;
+		int[][] array = new int[n][n];
+		
+		for ( int i = 0; i < n-2; i++ ){
+			for ( int j = 0; j < n-2; j++ ){
+				
+			}
+			for ( int j = 0; j < n-2; j++ ){
+							
+						}
+			for ( int j = 0; j < n-2; j++ ){
+				
+			}
+			for ( int j = 0; j < n-2; j++ ){
+				
+			}
+		}
+		
+		// 1 2 3
+		// 8 9 4
+		// 7 6 5
+	}
 }
